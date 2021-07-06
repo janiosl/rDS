9dadf576ee0a1261e84ea0d80c587d00957fb99c
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2021-07-06 09:09:47 -0300

    Create 01_meuPrimeiroRScript.r

---
+++ intro/rcc/01_meuPrimeiroRScript.r
@@ -0,0 +1,41 @@
+#Meu primeiro Script R
+print("Olá R")
+
+#Criação de variáveis
+numero1 <- 2
+numero2 <- 7
+
+#Execução das operações aritméticas básicas
+#Resultados armazenados em variáveis
+soma <- numero1 + numero2
+dife <- numero1 - numero2
+mult <- numero1 * numero2
+divi <- numero1 / numero2
+
+#Exibição dos resultados
+print(soma)
+print(dife)
+print(multi)
+print(divi)
+
+#Outras operações
+#Resultados exibidos no console
+#Exponenciação
+numero1 ** 2
+numero2 ** 2
+numero1 ** 3
+numero2 ** 3
+
+#Raiz quadrada
+sqrt(numero1)
+sqrt(numero2)
+
+#Operações com conjuntos de dados
+#Criação de um vetor de inteiros
+dados <- c(1, 2, 3, 4)
+
+#Cálculo de medidas dos dados
+mean(dados)   #Média
+sd(dados)     #Desvio padrão
+sum(dados)    #Soma dos dados
+
