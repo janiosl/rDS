bee41c019cc744de4ddf40605f79c76fbda46b0f
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2021-07-20 19:07:44 -0300

    Add files via upload

---
+++ intro/rcc/04_cargaDados_CSV.r
@@ -0,0 +1,31 @@
+##==================================================================
+##Carga de dados de arquivos CSV
+##Este estudo é uma continuação do código abaixo:
+##https://github.com/janiosl/rDS/blob/main/intro/rcc/03_cargaDados.r
+##==================================================================
+
+##==============================
+#Carga dos dados
+##==============================
+#Endereço do arquivo na internet
+arquivo <- "https://github.com/janiosl/python.ds/raw/master/data/petroleo.csv"
+
+#O endereço acima pode ser substituído pelo nome de um arquivo local
+#Para isso é preciso configurar o diretório de trabalho atual
+
+##======================
+#Leitura do arquivo CSV
+##======================
+dados <- read.csv(arquivo)
+
+
+##==============================
+#Conferência e análise dos dados
+##==============================
+#Conferência dos dados carregados
+View(dados)
+
+#Análise e tratamento dos dados
+mean(dados$INFLACAO)
+sum(dados$CUSTO_PETROLEO)
+dados$DATA <- NULL
