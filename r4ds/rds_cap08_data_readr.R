f304d8bc337cc1c68c8e0c06f5e9fced8f24ba5a
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2022-08-17 14:59:35 -0300

    Update files

--- r4ds/rds_cap08_data_readr.R
+++ r4ds/rds_cap08_data_readr.R
@@ -9,3 +9,30 @@ read_csv(
   1,2,3
   4,5,6"
 )
+
+meta <- read_csv("metadados.csv",
+                 skip = 2)
+meta
+
+comments <- read_csv("comments.csv",
+                     comment = "#")
+comments
+
+noTitle <- read_csv("noTitle.csv",
+                    col_names = FALSE)
+noTitle
+
+putTitle <- read_csv("noTitle.csv",
+                     col_names = c("x", "y", "z"))
+putTitle
+
+#Tratamento de valores ausentes
+#Sem tratamento
+read_csv("a,b,c\n1,2,.")
+read_csv("a,b,c\n1,2,-")
+
+
+#Com tratamento
+read_csv("a,b,c\n1,2,.", na = ".")
+read_csv("a,b,c\n1,2,-", na = "-")
+
