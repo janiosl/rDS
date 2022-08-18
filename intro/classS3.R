88e6a168d6464b3370a8f14474e2d4045700f888
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2022-08-18 17:37:18 -0300

    Update classS3.R

--- intro/classS3.R
+++ intro/classS3.R
@@ -1,5 +1,9 @@
 #Classes with S3
 
+##============================================================================
+##Creating a class
+##============================================================================
+#Constructor
 polygon <- function(n) {
   if (n <= 0) stop("Number of vertices should be greater than zero")
   obj <- list(n = n)
@@ -9,8 +13,69 @@ polygon <- function(n) {
   return(obj)
 }
 
-p <- polygon(0)
-p2 <- polygon(3)
 
-#Herança
-rectangle 
+#Inheritance
+rectangle <- function(w, h) {
+  obj <- polygon(4)
+  obj$w <- w
+  obj$h <- h
+  class(obj) <- append("rectangle", class(obj))
+  
+  return(obj)
+}
+
+
+square <- function(w) {
+  obj <- rectangle(w, w)
+  class(obj) <- append("square", class(obj))
+  
+  return(obj)
+}
+
+#Implementing an "interface" (methods)
+print.polygon <- function(obj) {
+  cat(obj$n, "\n")
+}
+
+print.rectangle <- function(obj) {
+  cat(obj$w, ",", obj$h, "\n")
+}
+
+
+area <- function(obj) {
+  UseMethod("area")
+}
+
+area.default <- function(obj) {
+  return(0)
+}
+
+area.rectangle <- function(obj) {
+  return(obj$w * obj$h)
+}
+
+area.square <- function(obj) {
+  return(obj$w * obj$h)
+}
+
+methods(class="default")
+
+
+##============================================================================
+##Using a class
+##============================================================================
+a <- 3
+p <- polygon(5)
+r <- rectangle(3, 10)
+s <- square(5)
+
+print(a)
+print(p)
+print(r)
+print(s)
+
+print(area(a))
+print(area(p))
+print(area(r))
+print(area(s))
+
