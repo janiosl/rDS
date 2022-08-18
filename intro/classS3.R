279f1f6997f6d2db034d99fd555b83220128c11f
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2022-08-18 14:04:22 -0300

    Update classS3.R

--- intro/classS3.R
+++ intro/classS3.R
@@ -1,10 +1,16 @@
 #Classes with S3
 
 polygon <- function(n) {
-  if (n < 0) stop("Number of vertices should be greater than zero")
+  if (n <= 0) stop("Number of vertices should be greater than zero")
   obj <- list(n = n)
   #Class can be set using class() or attr() function
   attr(obj, "class") <- "polygon"
   
   return(obj)
 }
+
+p <- polygon(0)
+p2 <- polygon(3)
+
+#Herança
+rectangle 
