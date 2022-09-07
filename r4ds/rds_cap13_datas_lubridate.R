69fc21787461e5652eebfd94a6c183edcf8d41e6
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2022-09-07 19:27:06 -0300

    Update files

--- r4ds/rds_cap13_datas_lubridate.R
+++ r4ds/rds_cap13_datas_lubridate.R
@@ -6,3 +6,29 @@ library(nycflights13)
 #Verificando tempos
 today() #Data
 now() #Data e hora
+
+#Lendo datas a partir de strings
+ymd("2017-01-31")
+mdy("January 31st, 2017")
+dmy("31-Jan-2017")
+
+#Lendo datas a partir de números
+ymd(20170131)
+
+#Datas e horas
+ymd_hms("2017-01-31 20:11:59")
+mdy_hm("01/31/2017 08:01")
+
+ymd(20170131, tz="UTC")
+
+#Exemplo a partir de um dataset com componentes individuais das datas
+flights %>% 
+  select(year, month, day, hour, minute)
+
+flights %>% 
+  select(year, month, day, hour, minute) %>% 
+  mutate(departure = make_datetime(year, month, day, hour, minute))
+
+flights %>% 
+  select(year, month, day, hour, minute) %>% 
+  mutate(departure = make_date(year, month, day))
