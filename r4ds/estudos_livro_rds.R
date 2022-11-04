e6bf0ccd123be56d896b78d84240fbe62e8f3289
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2022-11-04 20:07:19 -0300

    Add files via upload

---
+++ r4ds/estudos_livro_rds.R
@@ -0,0 +1,15 @@
+#Rodar cada capítulo e analisar saídas no console, enviroment e plots
+#Se quiser ver as saídas sem mistura com capítulos, reinicie a sessão antes de cada capítulo
+
+# Capítulo 19 -------------------------------------------------------------
+source("https://raw.githubusercontent.com/janiosl/rDS/main/r4ds/rds_cap19_modelos_constr.R",
+       echo = TRUE)
+
+source("https://raw.githubusercontent.com/janiosl/rDS/main/r4ds/rds_cap19_modelos_constr_fli.R",
+       echo = TRUE)
+
+# Capítulo 20 -------------------------------------------------------------
+source("https://raw.githubusercontent.com/janiosl/rDS/main/r4ds/rds_cap20_mult_modelos.R",
+       echo = TRUE)
+
+  
